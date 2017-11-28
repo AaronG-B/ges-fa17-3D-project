d32a4c94d30602bd948a2e62c13b0b26170e9632
Aaron G.B <aaron@gelblat-bronson.org>
2017-11-27 18:10:56 -0600

    end of class

--- GES3D Unity/Assets/Scripts/ActivatedLookedAtObjects.cs
+++ GES3D Unity/Assets/Scripts/ActivatedLookedAtObjects.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ActivatedLookedAtObjects : MonoBehaviour {
+    [SerializeField]
+    private float maxActiveDistance = 4.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +14,13 @@ public class ActivatedLookedAtObjects : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
-        Debug.DrawRay(transform.position, Vector3.forward);
-		
+        Debug.DrawRay(transform.position, Vector3.forward * maxActiveDistance, Color.blue);
+        RaycastHit raycastHit;
+
+        Physics.Raycast(transform.position, transform.forward, out raycastHit);
+
+        Iactivatable objectLookedAt = raycastHit.transform.GetComponent<Iactivatable>();
+
+
 	}
 }
